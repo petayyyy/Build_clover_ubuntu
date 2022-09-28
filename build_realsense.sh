13dada7d73018a5a6d5c4eca573fb9dfd9a55071
petayyyy <47917455+petayyyy@users.noreply.github.com>
2022-09-28 22:15:19 +0300

    Update build_realsense.sh

--- build_realsense.sh
+++ build_realsense.sh
@@ -1,3 +1,11 @@
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+
+sudo update-grub && sudo reboot
+
+sudo apt-get install git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
+
+sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
+
 git clone https://github.com/IntelRealSense/librealsense
 
 cd librealsense/
@@ -6,3 +14,6 @@ sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
+./scripts/patch-realsense-ubuntu-xenial-joule.sh
+
